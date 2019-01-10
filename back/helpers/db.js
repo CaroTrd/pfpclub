9e5eb71a6e99b4a79f9174997bcd5be8dfcb23fe
Carolina <carolinatirado92@gmail.com>
2019-01-10 13:20:53 +0100

    remise au propre du code

--- back/helpers/db.js
+++
@@ -1,9 +0,0 @@
-const mysql      = require('mysql');
-const connexion = mysql.createConnection({
-  host     : 'localhost',
-  port     : '8889',
-  user     : 'root',
-  password : 'root23',
-  database : 'pfp'
-});
-module.exports  =  connexion;
