13ce0bc0277f793cf3221d02bea82b468cd126f6
Joachim Bertrand <j.bertrand0032@gmail.com>
2019-01-23 10:39:57 +0100

    ajustement de App.js pour que ce soit correct avec eslint

--- front/src/App.js
+++ front/src/App.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
+        <p> App</p>
       </div>
     );
   }
 }
-
 export default App;
