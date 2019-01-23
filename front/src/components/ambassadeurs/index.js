851fc2ee7c6dbe0dee195567c2eb46ee3de5f2ee
Joachim Bertrand <j.bertrand0032@gmail.com>
2019-01-23 13:40:30 +0100

    first push ambassadeurs

---
+++ front/src/components/ambassadeurs/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class Ambassadeurs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Les Ambassadeurs </h1>
+        <p> Textes de presetation </p>
+      </div>
+    );
+  }
+}
+
+export default Ambassadeurs;
