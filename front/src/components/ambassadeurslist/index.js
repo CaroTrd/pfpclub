7f205cf8abd72cbd6e37571732927ad8c0e58ea4
Joachim Bertrand <j.bertrand0032@gmail.com>
2019-01-23 15:39:26 +0100

    premier push de ambassadeur lsit

---
+++ front/src/components/ambassadeurslist/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class AmbassadeursList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <p> Ambassadeurs list </p>
+      </div>
+    );
+  }
+}
+
+export default AmbassadeursList;
